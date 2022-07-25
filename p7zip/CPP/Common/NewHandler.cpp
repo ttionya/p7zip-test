d3d015e153cb86c7eaab7c2d642e4796ae82f849
ttionya <git@ttionya.com>
2022-07-25 21:38:19 +0800

    chore: add p7zip source code

---
+++ p7zip/CPP/Common/NewHandler.cpp
@@ -0,0 +1,51 @@
+// NewHandler.cpp
+ 
+#include "StdAfx.h"
+
+#include "../../C/Alloc.h"
+
+#include <exception> 
+
+#include <new>
+
+
+#ifdef DONT_REDEFINE_NEW
+
+int g_NewHandler = 0;
+
+#else
+
+/* An overload function for the C++ new */
+void * operator new(size_t size) throw(std::bad_alloc)
+{
+  void *p;
+  p = MyAlloc(size);
+  if(!p)
+    throw std::bad_alloc();
+  return p;
+}
+
+/* An overload function for the C++ new[] */
+void * operator new[](size_t size) throw(std::bad_alloc)
+{
+  void *p;
+  p = MyAlloc(size);
+  if(!p)
+    throw std::bad_alloc();
+  return p;
+}
+
+/* An overload function for the C++ delete */
+void operator delete(void *pnt) throw()
+{
+    MyFree(pnt);
+}
+
+/* An overload function for the C++ delete[] */
+void operator delete[](void *pnt) throw()
+{
+    MyFree(pnt);
+}
+
+#endif
+
